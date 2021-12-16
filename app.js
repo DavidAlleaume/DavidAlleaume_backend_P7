827b19dbc0c5841154a5deb45ed120185668fba8
David Alleaume <david.alleaume@gmail.com>
2021-12-16 17:19:16 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+app.get('/', (req, res) => { res.status(200).send('Bonjour et bienvenue sur mon serveur !')})
+
+//const userRoutes = require('./routes/user.routes')
+//const messageRoutes = require('./routes/message.routes')
+
+//app
+    //.use('/api/user', userRoutes)
+    //.use('/api/message', messageRoutes)
+
+
+module.exports = app
